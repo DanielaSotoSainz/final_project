819953002af0cac8841b1fc428aa4a7e19cfc732
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2022-12-02 00:17:57 -0600

    Added scripts folder to database, fixed stay_days

---
+++ database/scripts/fix_stay_days.py
@@ -0,0 +1,26 @@
+"""
+Utility script to fix non-clean data.
+
+run from the root directory:
+
+    python database/scripts/fix_stay_days.py
+"""
+import pandas as pd
+from pathlib import Path
+
+
+def replace_negative_numbers_with_mean(
+    colname: str = "stay_days",
+    files: list = [
+        "./database/clean2/db_patient.csv",
+    ]
+):
+    for file in files:
+        df = pd.read_csv(Path(file).resolve())
+        mean = df[colname][df[colname] >= 0].describe()['mean']
+        df[colname] = df[colname].map(lambda x: x if x > 0 else round(mean))
+        df.to_csv(file, index=False)
+
+
+if __name__ == "__main__":
+    replace_negative_numbers_with_mean()
