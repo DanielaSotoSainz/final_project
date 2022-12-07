8b948b32d3053e051f73dc3113de0afd4324d031
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2022-12-06 20:24:06 -0600

    quick navBar fix

--- docs/static/js/navBar.js
+++ docs/static/js/navBar.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $.get(
-        "/static/html/navbar.html", 
+        "./static/html/navbar.html", 
         (html_string) => {
             let navbar = document.getElementById("navbar");
             navbar.innerHTML = html_string
